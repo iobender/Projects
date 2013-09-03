d220b434fbbff7504b94af51a6de5022de1d7630
iobender <me@iobender.com>
2013-09-03 19:55:26 -0400

    fibonacci.py returns correct results now, in O(n) time. Updated README

--- Numbers/fibonacci/fibonacci.py
+++ Numbers/fibonacci/fibonacci.py
@@ -3,9 +3,17 @@
 import sys
 
 quiet= False;
+all= False;
 for arg in sys.argv:
-	if(arg == "-q" or arg == "--quiet"):
+	if(arg == "--quiet"):
 		quiet= True
+	if(arg == "--all"):
+		all= True
+	if(arg[0] == '-'):
+		if("q" in arg):
+			quiet= True
+		if("a" in arg):
+			all= True
 
 if not(quiet):
 	sys.stdout.write("Program to find the nth Fibonacci number.\nEnter n: ")
@@ -21,4 +29,24 @@ def validate(n):
 
 validate(user_input)
 n= int(user_input)
+
+def fib(n):
+	if n == 0:
+		return [0]
+	elif n == 1:
+		return [0, 1]
+	else:
+		result= [0, 1]
+		first= 0
+		second= 1
+		for i in range(2, n + 1):
+			temp= second
+			second= first + second
+			first= temp
+			result.append(second)
+		return result
+
+fib_n= fib(n)
+print(fib_n)
+
 
