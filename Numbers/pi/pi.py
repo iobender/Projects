eac80ff7d1c792ba65a51f375af14db00cd0e735
iobender <me@iobender.com>
2013-09-03 22:18:14 -0400

    Saved pi.py

--- Numbers/pi/pi.py
+++ Numbers/pi/pi.py
@@ -7,7 +7,7 @@ for arg in sys.argv:
 	if(arg == "-q" or arg == "--quiet"):
 		quiet= True
 if not(quiet):
-	sys.stdout.write("Program to find the nth digit of pi\nEnter n: ")
+	sys.stdout.write("Program to find the first n digits of pi.\nEnter n: ")
 user_input= sys.stdin.readline()
 
 def valid_input(n):
@@ -22,4 +22,10 @@ if valid_input(user_input):
 else:
 	sys.exit("" if quiet else "n must be a number")
 
-print(n)
+result= 0
+for i in range(n + 1):
+	step= (4/(8*i + 1) - 2/(8*i + 4) - 1/(8*i + 5) - 1/(8*i + 6))
+	print(str(i) + ": " + str(step))
+	result += step
+print(result)
+
