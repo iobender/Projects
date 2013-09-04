d68eac45b50c970683924ffaed92fb071bf95de0
iobender <me@iobender.com>
2013-09-04 12:21:15 -0400

    Finished next_prime. Prints primes separated by spaces until interrupted

--- Numbers/next_prime/next_prime.py
+++ Numbers/next_prime/next_prime.py
@@ -2,4 +2,19 @@
 
 import sys
 
+primes= [2]
+sys.stdout.write("2")
+
+def is_prime(n):
+	for prime in primes:
+		if n % prime == 0:
+			return False
+	return True;
+
+n= 3
+while True:
+	if is_prime(n):
+		primes.append(n)
+		sys.stdout.write(" " + str(n))
+	n += 2
 
